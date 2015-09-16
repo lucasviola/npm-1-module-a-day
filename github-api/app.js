8ce5cc1468d6705f93a9b17d17e3bbe274b23dde
lucasviola <lucasmatzenbacher@gmail.com>
2015-09-16 16:13:26 -0300

    basic repo info retrieved

--- github-api/app.js
+++ github-api/app.js
@@ -1,1 +1,9 @@
-var github = require('../node_modules/github-api');
+var GithubStore = require('../node_modules/github-api');
+
+var github = new GithubStore({apiUrl : "https://api.github.com"});
+
+var repo = github.getRepo("lucasviola", "easyshell");
+
+repo.show(function(err, repo){});
+
+console.log(repo);
